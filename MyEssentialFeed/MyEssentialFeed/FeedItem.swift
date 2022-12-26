f9dfb2a933a7d8b9652e30f7ecc7394de3be7b11
Aymen Aifa <mohamedaymen.afia@esprit.tn>
2022-12-26 20:36:50 +0100

    create FeedItem

---
+++ MyEssentialFeed/MyEssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  MyEssentialFeed
+//
+//  Created by Aymen Aifa on 26.12.22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let desciption: String?
+    let location: String?
+    let imageURL: URL
+}
---
+++ MyEssentialFeed/MyEssentialFeed/MyEssentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  MyEssentialFeed.h
+//  MyEssentialFeed
+//
+//  Created by Aymen Aifa on 26.12.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyEssentialFeed.
+FOUNDATION_EXPORT double MyEssentialFeedVersionNumber;
+
+//! Project version string for MyEssentialFeed.
+FOUNDATION_EXPORT const unsigned char MyEssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyEssentialFeed/PublicHeader.h>
+
+
